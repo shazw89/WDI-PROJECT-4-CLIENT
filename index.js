d85759934eba0a69f6ce51762ece5955230d2245
remyce <remycewilsonhughes@gmail.com>
2017-02-19 14:58:34 +0000

    tried to add angular module

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express    = require('express');
-const expressJWT = require('express-jwt');
+const angularJWT = require('angular-jwt');
 const port       = process.env.PORT || 3000;
 const app        = express();
 const dest       = `${__dirname}/public`;
 
 app.use(express.static(dest));
 
-app.use('/api', expressJWT({ secret: secret })
+app.use('/api', angularJWT({ secret: secret })
   .unless({
     path: [
       { url: '/api/login', methods: ['POST'] },
