bd869c7fac0717bc0b96bea8c24f31ce4ef84fd7
remyce <remycewilsonhughes@gmail.com>
2017-02-19 14:46:19 +0000

    added dependancies to package.json

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const port    = process.env.PORT || 3000;
-const app     = express();
-const dest    = `${__dirname}/public`;
+const express    = require('express');
+const expressJWT = require('express-jwt');
+const port       = process.env.PORT || 3000;
+const app        = express();
+const dest       = `${__dirname}/public`;
 
 app.use(express.static(dest));
+
+app.use('/api', expressJWT({ secret: secret })
+  .unless({
+    path: [
+      { url: '/api/login', methods: ['POST'] },
+      { url: '/api/register', methods: ['POST'] }
+    ]
+  }));
 
 app.get('/*', (req, res) => res.sendFile(`${dest}/index.html`));
 
 app.listen(port, () => console.log(`Express has started on port: ${port}`));
-
-
-// beyonce rules 
---
+++ src/js/configs/router.config.js
@@ -0,0 +1,12 @@
+angular
+  .module('Synesthesia')
+  .config(Router);
+
+Router.$inject = ['$stateProvider', '$locationProvider', '$urlRouterProvider'];
+function Router($stateProvider, $locationProvider, $urlRouterProvider) {
+  $locationProvider.html5Mode(true);
+
+
+
+
+}
