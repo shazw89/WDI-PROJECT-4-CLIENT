cf01ecfe95681607886710101b8bef5ec2070496
shazw89 <shyan.wong@gmail.com>
2017-02-22 20:50:46 +0000

    Completed navbar and almost completed burger menu

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -18,4 +18,10 @@ function MainCtrl($rootScope, CurrentUserService, $state) {
     vm.user = null;
     $state.go('login');
   });
+
+  window.onload = function () {
+    document.getElementById('burger-container').addEventListener('click', function () {
+      this.classList.toggle('open');
+    });
+  };﻿
 }
